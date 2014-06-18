28fe6c370fa475e8f7db1fbb5bd228517a94d77f
Steven Lazatin <steven@Steven-Lazatins-MacBook-Pro.local>
2014-06-17 17:08:44 -0700

    Mapping all the Markers on goolde ma; not filtered

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,6 +1,9 @@
 class Marker < ActiveRecord::Base
   belongs_to :interest_group
   geocoded_by :address
+  after_validation :geocode
+  reverse_geocoded_by :latitude, :longitude
+after_validation :reverse_geocode
 
   def self.nearme(loc)
 
