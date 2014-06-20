092cddb55ca18820e3d4e563a74feb238fa742f2
Mallory Reulman <mreulman@gmail.com>
2014-06-20 10:59:10 -0700

    fixed migration errors

--- db/migrate/20140619215617_remove_user_id_from_interest_groups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromInterestGroups < ActiveRecord::Migration
-  def change
-    remove_column :interest_groups, :user_id, :integer
-  end
-end
--- db/migrate/20140619220620_create_join_table_user_interest_group.rb
+++
@@ -1,4 +0,0 @@
-class CreateJoinTableUserInterestGroup < ActiveRecord::Migration
-  def change
-  end
-end
