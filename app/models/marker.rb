b5a65657a670f8d11abffe36b293197f74136fe2
Mallory Reulman <mreulman@gmail.com>
2014-06-25 14:58:23 -0700

    horizontal image fix Part 2

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -2,7 +2,7 @@ class Marker < ActiveRecord::Base
 
 validates_presence_of :title, :description, :interest_group_id, :address
 
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :convert_options => { :all => '-auto-orient' } }, :default_url => "missing.jpg"
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :convert_options => '-auto-orient' }, :default_url => "missing.jpg"
 
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
